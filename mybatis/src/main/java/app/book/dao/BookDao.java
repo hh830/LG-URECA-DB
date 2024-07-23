d8eb885f3678a21c243e05d79bd5c879ddad6264
hh830 <jhh8890@naver.com>
2024-07-24 00:27:40 +0900

    swing jdbc 코드 mybatis 사용하여 리팩토링

---
+++ mybatis/src/main/java/app/book/dao/BookDao.java
@@ -0,0 +1,26 @@
+package app.book.dao;
+
+import app.book.dto.BookDto;
+import org.apache.ibatis.annotations.Delete;
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Select;
+import org.apache.ibatis.annotations.Update;
+
+import java.util.List;
+
+// mapper : java method - sql
+public interface BookDao {
+    @Select("select bookid bookId, bookname bookName, publisher, price from book;")
+    List<BookDto> listBook();
+    @Select("select bookid bookId, bookname bookName, publisher, price from book where bookname like concat('%', #{searchWord}, '%') ")
+    List<BookDto> listBookSearch(String searchWord);
+    @Select("select bookid bookId, bookname bookName, publisher, price from book where bookid = #{bookId}; ")
+
+    BookDto detailBook(int bookId);
+    @Insert("insert into book (bookid, bookname, publisher, price) values ( #{bookId}, #{bookName}, #{publisher}, #{price} ); ")
+    int insertBook(BookDto bookDto);
+    @Update("insert into book (bookid, bookname, publisher, price) values ( #{bookId}, #{bookName}, #{publisher}, #{price} ); ")
+    int updateBook(BookDto bookDto);
+    @Delete("delete from book where bookid = #{bookId}")
+    int deleteBook(int bookId);
+}
